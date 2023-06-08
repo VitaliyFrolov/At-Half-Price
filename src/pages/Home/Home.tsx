da1620e3f626d0dc405ff78a6df9208c157e25c0
Vii <vitaliy.frolov.2001@mail.ru>
2023-06-08 20:23:44 +0300

    add json server

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AllStores } from './components/AllStores/AllStores';
 import { TopDiscount } from './components/TopDiscounts/TopDiscount';
 import { PageBtn } from '../../shared/PageBtn/PageBtn';
+import axios from 'axios';
 // import { Link } from 'react-router-dom';
 
 
 export const Home = () => {
+    const [discountCard, setDiscountCard] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/posts')
+            .then((res) => setDiscountCard(res.data))
+    }, []);
+
     return (
         <>
             <AllStores />
-            <PageBtn text='Смотреть все' />
-            <TopDiscount />
-            <PageBtn text='Смотреть все' />
+            <PageBtn
+                text='Смотреть все' 
+            />
+            <TopDiscount
+                items={discountCard}
+            />
+            <PageBtn
+                text='Смотреть все' 
+            />
         </>
     )
 };
--- src/pages/Home/components/TopDiscounts/TopDiscount.tsx
+++ src/pages/Home/components/TopDiscounts/TopDiscount.tsx
@@ -2,29 +2,26 @@ import React from "react";
 import { TopDiscountCard } from "../TopDiscountCard/TopDiscountCard";
 import style from './TopDiscount.module.scss';
 
-// interface Props {
+interface Props {
+    items: any
+}
 
-// }
-
-export const TopDiscount = () => {
+export const TopDiscount = ({items}: Props) => {
+    console.log(items)
     return (
         <section className={style.container}>
             <h2 className={style.title}>
                 Самые выгодные скидки сейчас
             </h2>
             <div className={style.content}>
-               <TopDiscountCard price={10} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={13} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={14} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={11} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={12} product={'продукт'} shop={'магазин'} discount={15} />
-               <TopDiscountCard price={33} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={11} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={10} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={14} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={15} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={11} product={'продукт'} shop={'магазин'} discount={10} />
-               <TopDiscountCard price={44} product={'продукт'} shop={'магазин'} discount={10} /> 
+                {items.map((item: any) => (
+                    <TopDiscountCard 
+                        price={item.price}
+                        product={item.product}
+                        shop={item.shop}
+                        discount={item.discount}
+                    />
+                ))}
             </div>
         </section>
     )

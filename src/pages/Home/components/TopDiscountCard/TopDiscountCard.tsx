08a3cea724b4b3f16d3519f4328e37907835d998
Vii <vitaliy.frolov.2001@mail.ru>
2023-06-13 14:48:27 +0300

    add check for props 'discount' and render sale in the 'TopDiscountCard' module

--- src/pages/Home/components/TopDiscountCard/TopDiscountCard.tsx
+++ src/pages/Home/components/TopDiscountCard/TopDiscountCard.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import style from './TopDiscountCard.module.scss';
 
 interface Props {
-    price: number
-    product: string
-    shop: string
-    discount: number
-}
+    price: number;
+    product: string;
+    shop: string;
+    discount?: number;
+};
 
-const discountCalc = (price: number, discount: number) => {
+const calcDiscount = (price: number, discount?: number): number => {
+   if (discount) {
     const percent = price * discount / 100;
     const sale = price - percent;
     return sale;
+   } else {
+    return price
+   }
 };
+
+
 
 
 export const TopDiscountCard = ({price, product, shop, discount}: Props) => {
@@ -20,24 +26,27 @@ export const TopDiscountCard = ({price, product, shop, discount}: Props) => {
         <div className={style.container}>
             <div className={style.img}>
                 <img src="/img/BannerTestImg.png" width={31} height={18} alt='sale' />
-                <div className={style.saleBar}>
-                    <span>
-                        <p className={style.saleBarText}>
-                            -{discount}%
-                        </p>
-                    </span>
-                </div>
+                {discount ? 
+                    <div className={style.saleBar}>
+                        <span>
+                            <p className={style.saleBarText}>
+                                -{discount}%
+                            </p>
+                        </span>
+                    </div> : null
+                }
             </div>
             <div className={style.content}>
                 <div className={style.priceContainer}>
                     <div className={style.price}>
                         <span className={style.priceNum}>
-                            {discountCalc(price, discount)}
+                            {calcDiscount(price, discount)}
                         </span>
                         <span className={style.priceСurrency}>
                             NIS
                         </span> 
                     </div>
+                    {discount ?
                     <div className={style.sale}>
                         <span className={style.saleNum}>
                             <s>
@@ -49,7 +58,8 @@ export const TopDiscountCard = ({price, product, shop, discount}: Props) => {
                                 NIS
                             </s>
                         </span>
-                    </div>
+                    </div> : null
+                    }
                 </div>
                 <p className={style.product}>
                     {product}
@@ -60,4 +70,4 @@ export const TopDiscountCard = ({price, product, shop, discount}: Props) => {
             </div>
         </div>
     )
-}
+};
--- src/pages/Home/components/TopDiscounts/TopDiscount.tsx
+++ src/pages/Home/components/TopDiscounts/TopDiscount.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { TopDiscountCard } from "../TopDiscountCard/TopDiscountCard";
 import style from './TopDiscount.module.scss';
 
+interface Item {
+    price: number;
+    product: string;
+    shop: string;
+    discount?: number;
+}
+
 interface Props {
-    items: any
+    items: Item[]
 }
 
 export const TopDiscount = ({items}: Props) => {
-    console.log(items)
     return (
         <section className={style.container}>
             <h2 className={style.title}>
                 Самые выгодные скидки сейчас
             </h2>
             <div className={style.content}>
-                {items.map((item: any) => (
+                {items.map((item: Item) => (
                     <TopDiscountCard 
                         price={item.price}
                         product={item.product}
